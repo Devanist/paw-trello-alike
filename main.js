c501f69433349b586d5d77a32ba727f717158a37
Devanist <devan93@gmail.com>
2016-10-09 18:55:20 +0200

    react-router get to work

--- main.js
+++ main.js
@@ -4,14 +4,27 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './Components/App';
 import Reducer from './Reducer';
+import {Router, Route, IndexRoute, Link, hashHistory} from 'react-router';
+
+import Home from './Components/Views/Home';
+import Register from './Components/Views/Register';
+import Login from './Components/Views/Login';
 
 import style from './Styles/Layout.scss';
 
 var store = createStore(Reducer);
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    (   
+        <Provider store={store}>
+            <Router history={hashHistory}>
+                <Route path="/" component={App}>
+                    <IndexRoute component={Home} />
+                    <Route path="register" component={Register}/>
+                    <Route path="login" component={Login}/>
+                </Route>
+            </Router>
+        </Provider>
+    ),
     document.getElementById('app')
 );
