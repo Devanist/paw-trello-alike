e46e2e23982ee1e01206cdc9cd67ff69c7830063
Devanist <devanist@outlook.com>
2016-10-18 14:56:09 +0200

    updated webpack config template

--- webpack.config.template.js
+++ webpack.config.template.js
@@ -1,8 +1,12 @@
+var webpack = require('webpack');
+var PROD = process.argv.indexOf('--prod') !== -1;
+
 module.exports = {
     entry: "PATH_TO_main.js",
+    devtool: 'source-map',
     output: {
-        path: __dirname,
-        filename: "bundle.js",
+        path: PROD ? "./OUTPUT/" : __dirname,
+        filename:  PROD ? "bundle.min.js" : "bundle.js",
     },
     module: {
         loaders: [
@@ -11,11 +15,18 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: '/node_modules/',
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react', 'es2015'],
+                    plugins: ['transform-object-rest-spread']
                 }
             },
             {test: /\.json$/, loader: "json-loader"},
-            {test: /\.scss$/, loader: "style!css!sass"}
+            {test: /\.scss$/, loader: "style!css!sass"},
+            {test: /\.png$/, loader: "url-loader"}
         ]
-    }
+    },
+    plugins: PROD ? [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false}
+        })
+    ] : []
 };
